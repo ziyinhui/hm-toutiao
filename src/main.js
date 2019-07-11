96b8f81076b2ea8d557f75f1c5c88c5d865550cb
zhousg <zhoushugang@itcast.cn>
2019-07-11 17:17:03 +0800

    完成登录

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 2. 在使用vue-cli的时候 @符号在路径中作为前缀使用的时候 代表src目录
 import router from '@/router'
 
+// 导入axios
+import axios from 'axios'
+Vue.prototype.$http = axios
+
 Vue.use(ELEMENT)
 
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,89 @@
 <template>
-  <div class='login-container'>
+  <div class="login-container">
     <el-card class="login-box">
-      <img src="../../assets/images/logo_index.png" alt="">
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 登录表单 -->
+      <el-form ref="loginForm" :status-icon="true" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary" @click="login()">登 录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    // 校验手机号
+    const checkMobile = (rule, value, callback) => {
+      // 校验逻辑   把value拿出来进行手机号的格式校验
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不正确'))
+      }
+    }
+    return {
+      // 表单数据对象
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      // 表单验证规则对象
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 对整个表单进行校验
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 提交登录请求  axios是基于primise封装的 post() 返回值一个promise对象
+          this.$http
+            .post(
+              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+              this.loginForm
+            )
+            .then(res => {
+              // res 是 响应对象  包含 后台返回的数据 res.data
+              // console.log(res.data) 去做什么事情？？？
+              // 1. 跳转到首页
+              // TODO 2. 保存用户的信息  用来判断登录的状态
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
-.login-container{
+.login-container {
   width: 100%;
   height: 100%;
   position: absolute;
@@ -19,15 +91,15 @@ export default {}
   top: 0;
   // cover 铺面容器 多余的裁剪   contain 在容器内容完全显示图片
   // 背景图定位 / 背景图尺寸
-  background:url(../../assets/images/login_bg.jpg) no-repeat center / cover;
-  .login-box{
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  .login-box {
     width: 400px;
-    height: 300px;
+    height: 330px;
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
-    img{
+    transform: translate(-50%, -50%);
+    img {
       display: block;
       width: 200px;
       margin: 10px auto;
