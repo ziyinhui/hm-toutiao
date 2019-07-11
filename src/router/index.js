39419b12d25cf8e598f7e22d01aa2671cc8fe87a
zhousg <zhoushugang@itcast.cn>
2019-07-11 18:40:36 +0800

    首页-布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/', component: Home }
   ]
 })
 
